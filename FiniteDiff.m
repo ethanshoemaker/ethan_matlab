e2677cc81909326fb0d8e7c0dc8fb5af6d20c001
Ethan Shoemaker <89482507+ethanshoemaker@users.noreply.github.com>
2021-11-21 14:28:56 -0700

    Add files via upload

--- FiniteDiff.m
+++ FiniteDiff.m
@@ -17,36 +17,39 @@ T_max = [];
 T_avg = [];
 
 
-for N = 5:5:100
+for N = 30
 
     delta_x = x_dim/N; %delta_x
     delta_y = y_dim/N; %delta_y
     x = 0:1/(N-1):1;
     y = x;
     [X,Y] = meshgrid(x,y);
-    F = zeros(N*N,1);
- 
-
-    %B = N+1:N:(N*N)-((N-1)+N);
-    %C = [2:1:N-1];
-    D = N+2:1:2*N-1;
-    G = [];
-
-    for i = 1:N-3
-        E = (D(1)+i*N):1:(D(N-2)+i*N);
-        D = [D E];
-    end
-
     A = eye(N*N);
+    B = zeros(N*N,1);
 
-    F(1,1) = 0.25*(W_surface)+0.25*(N_surface);
+    %%%BOUNDARY CONDITIONS%%%
 
-    for i = N+1:N:(N*N)-((N-1)+N)
-        F(i,1) = .25*W_surface;
+    B(1,1) = 0.25*(W_surface)+0.25*(N_surface); %top left node
+
+    B((N*N)-(N-1),1) = (1/3)*W_surface; %bottom left node
+
+    B(N,1) = (1/3)*N_surface; %top right node
+
+    for i = N+1:N:(N*N)-((N-1)+N) %left side non-corner
+        B(i,1) = .25*W_surface;
     end
 
-    for i = 2:1:N-1
-        F(i,1) = .25*N_surface;
+    for i = 2:1:N-1 %top side non-corner
+        B(i,1) = .25*N_surface;
+    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    %%%NODE DEPENDENCIES%%%
+
+    %%%interior nodes%%%
+    D = N+2:1:2*N-1;
+    for i = 1:N-3
+        D = [D, (D(1)+i*N):1:(D(N-2)+i*N)];
     end
 
     for i = D
@@ -54,62 +57,57 @@ for N = 5:5:100
         A(i,i+N) = -0.25;
         A(i,i-N) = -0.25;
         A(i,i-1) = -0.25;
-    end
+    end  
 
-    F((N*N)-(N-1),1) = (1/3)*W_surface;
-    F(N,1) = (1/3)*N_surface;
-
-    %top left node
+    %%%top left node%%%
     A(1,2) = -0.25;
     A(1,N+1) = -0.25;
 
-    %top right node
+    %%%top right node%%%
     A(N,N-1) = -1/3;
     A(N, N*2) = -1/3;
 
-    %bottom left node
+    %%%bottom left node%%%
     A(1+(N*(N-1)),1+(N*(N-1))-N) = -1/3;
     A(1+(N*(N-1)),2+(N*(N-1))) = -1/3;
 
-    %bottom right node
+    %%%bottom right node%%%
     A(N*N,(N*N)-1) = -0.5;
     A(N*N,(N*N)-N) = -0.5;
 
-    %top row non-corner nodes
 
+    %%%top row non-corner nodes%%%
     for i = 2:N-1
         A(i,i-1) = -0.25;
         A(i,i+1) = -0.25;
         A(i,i+N) = -0.25;
     end
 
-    %bottom row non-corner nodes
 
+    %%%bottom row non-corner nodes%%%
     for i = N*N-(N-2):(N*N)-1
         A(i,i-1) = -1/3;
         A(i,i+1) = -1/3;
         A(i,i-N) = -1/3;
     end
 
-    %right side non-corner nodes
-
+    %%%right side non-corner nodes%%%
     for i = 2*N:N:(N*N)-N
         A(i,i-N) = -1/3;
         A(i,i-1) = -1/3;
         A(i,i+N) = -1/3;
     end
 
-    %left side non-corner nodes
-
+    %%%left side non-corner nodes%%%
     for i = 1+N:N:(N*N)-N-(N-1)
         A(i,i+1) = -0.25;
         A(i,i-N) = -0.25;
         A(i,i+N) = -0.25;
     end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    Z = A\F;
-
-    T = reshape(Z,N,N);
+    T = A\B;
+    T = reshape(T,N,N);
     T_heatmap = flip(flip(T),2);
 
 
@@ -140,17 +138,17 @@ for N = 5:5:100
     ylabel('y-distance (m)')
 
     
-    tmax = max(T,[],'all');
-    T_max = [T_max tmax];
+    %tmax = max(T,[],'all');
+    %T_max = [T_max tmax];
     
 
     %tavg = mean(T,'all');
     %T_avg = [T_avg tavg];
 end
 
-j = 5:5:100;
-figure();
-plot(j,T_max)
-xlabel('# of Nodes in Each Direction')
-ylabel('Max Temperature (K)')
-title('Max Temperature (K) vs # of Nodes in Each Direction')
+%j = 5:5:100;
+%figure();
+%plot(j,T_max)
+%xlabel('# of Nodes in Each Direction')
+%ylabel('Max Temperature (K)')
+%title('Max Temperature (K) vs # of Nodes in Each Direction')
